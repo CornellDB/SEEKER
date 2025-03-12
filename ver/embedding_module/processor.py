9297c972e84ba7126d0e971d47b34f20158d25bf
David Han <dmh338@cornell.edu>
2025-03-11 22:20:00 -0400

    add parallilization to embedding model

--- ver/embedding_module/processor.py
+++ ver/embedding_module/processor.py
@@ -1,24 +1,40 @@
 import csv
 import os
 import glob
+from concurrent.futures import ThreadPoolExecutor
 
 class Processor:
     def __init__(self, directory_path):
         self.directory_path = directory_path
 
-    def process(self):
+    def process_file(self, file_path):
         sentences = []
-
-        for file_path in glob.glob(os.path.join(self.directory_path, '*.csv')):
-            with open(file_path, mode='r') as file:
-                csv_reader = csv.reader(file)
-                headers = next(csv_reader)
-                sentences.append(headers)
-                columns = {header: [] for header in headers}
-                
-                for row in csv_reader:
+        with open(file_path, mode='r') as file:
+            csv_reader = csv.reader(file, quoting=csv.QUOTE_NONE)
+            headers = next(csv_reader)
+            sentences.append(headers)
+            columns = {header: [] for header in headers}
+            
+            for row in csv_reader:
+                try:
                     for header, value in zip(headers, row):
                         columns[header].append(value)
-                for column in columns.values():
-                    sentences.append(column)
+                except:
+                    # handling field larger than field limit
+                    pass
+                
+            for column in columns.values():
+                sentences.append(column)
+        return sentences
+
+    def process(self, max_workers=4):
+        sentences = []
+        file_paths = glob.glob(os.path.join(self.directory_path, '*.csv'))
+        
+        with ThreadPoolExecutor(max_workers=max_workers) as executor:
+            results = executor.map(self.process_file, file_paths)
+        
+        for result in results:
+            sentences.extend(result)
+        
         return sentences
