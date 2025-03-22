683bbb118d298c7e4d0fe7174ffb8b8ab99d6c07
David Han <dmh338@cornell.edu>
2025-03-22 02:36:13 -0400

    modify processor and add transform

--- ver/embedding_module/processor.py
+++ ver/embedding_module/processor.py
@@ -1,7 +1,9 @@
 import csv
 import os
 import glob
-from concurrent.futures import ThreadPoolExecutor
+from pathos.multiprocessing import ProcessingPool as Pool
+# from multiprocessing.pool import ThreadPool as Pool
+from tqdm import tqdm
 
 class Processor:
     def __init__(self, directory_path):
@@ -10,29 +12,42 @@ class Processor:
     def process_file(self, file_path):
         sentences = []
         with open(file_path, mode='r') as file:
-            csv_reader = csv.reader(file, quoting=csv.QUOTE_NONE)
-            headers = next(csv_reader)
-            sentences.append(headers)
-            columns = {header: [] for header in headers}
-            
-            for row in csv_reader:
-                try:
-                    for header, value in zip(headers, row):
-                        columns[header].append(value)
-                except:
-                    # handling field larger than field limit
-                    pass
+            try:
+                csv_reader = csv.reader(file, quoting=csv.QUOTE_NONE)
+                headers = next(csv_reader)
+                sentences.append(headers)
+                columns = {header: [] for header in headers}
                 
-            for column in columns.values():
-                sentences.append(column)
+                for row in csv_reader:
+                    try:
+                        for header, value in zip(headers, row):
+                            columns[header].append(value)
+                    except:
+                        # handling field larger than field limit
+                        pass
+                    
+                for column in columns.values():
+                    sentences.append(column)
+
+            except:
+                pass
+
+        return sentences
+    
+    # single-threaded version
+    def process_single(self, maxlen=1000):
+        sentences = []
+        file_paths = glob.glob(os.path.join(self.directory_path, '*.csv'))
+        for i in tqdm(range(min(maxlen, len(file_paths)))):
+            sentences.extend(self.process_file(file_paths[i]))
         return sentences
 
-    def process(self, max_workers=4):
+    def process(self, max_workers=2):
         sentences = []
         file_paths = glob.glob(os.path.join(self.directory_path, '*.csv'))
         
-        with ThreadPoolExecutor(max_workers=max_workers) as executor:
-            results = executor.map(self.process_file, file_paths)
+        with Pool(processes=max_workers) as pool:
+            results = tqdm(pool.imap(self.process_file, file_paths), total=len(file_paths))
         
         for result in results:
             sentences.extend(result)
--- ver/transform_module/transform.py
+++ ver/transform_module/transform.py
@@ -1,5 +1,9 @@
 from typing import List
 from qbe_module.query_by_example import ExampleColumn
+from datetime import datetime
+import inflect
+from nltk.stem import PorterStemmer
+
 
 class Transform:
     def __init__(self, example_columns: List[ExampleColumn], transform_function):
@@ -43,3 +47,67 @@ class DateTransform(Transform):
         return [ExampleColumn(attr=mm, examples=column.examples) for mm in month_map[column.attr]]
                 
 
+class DateFormatTransform(Transform):
+    def __init__(self, example_columns: List[ExampleColumn], transform_function=None):
+        if transform_function is None:
+            transform_function = self.default_date_format_transform
+        super().__init__(example_columns, transform_function)
+
+    def default_date_format_transform(self, column: ExampleColumn):
+        date_formats = [
+            "%Y-%m-%d",
+            "%d-%m-%Y",
+            "%m/%d/%Y",
+            "%B %d, %Y",
+            "%d %B %Y"
+        ]
+
+        transformed_columns = []
+        for date_format in date_formats:
+            try:
+                date_obj = datetime.strptime(column.attr, date_format)
+                for fmt in date_formats:
+                    transformed_columns.append(ExampleColumn(attr=date_obj.strftime(fmt), examples=column.examples))
+            except ValueError:
+                continue
+
+        return transformed_columns
+    
+class NumberToWordTransform(Transform):
+    def __init__(self, example_columns: List[ExampleColumn], transform_function=None):
+        if transform_function is None:
+            transform_function = self.default_number_to_word_transform
+        super().__init__(example_columns, transform_function)
+
+    def default_number_to_word_transform(self, column: ExampleColumn):
+        p = inflect.engine()
+        try:
+            num = int(column.attr)
+            return [ExampleColumn(attr=p.number_to_words(num), examples=column.examples)]
+        except ValueError:
+            return []
+
+class WordToNumberTransform(Transform):
+    def __init__(self, example_columns: List[ExampleColumn], transform_function=None):
+        if transform_function is None:
+            transform_function = self.default_word_to_number_transform
+        super().__init__(example_columns, transform_function)
+
+    def default_word_to_number_transform(self, column: ExampleColumn):
+        p = inflect.engine()
+        try:
+            num = p.words_to_number(column.attr)
+            return [ExampleColumn(attr=str(num), examples=column.examples)]
+        except ValueError:
+            return []
+        
+class StemmingTransform(Transform):
+    def __init__(self, example_columns: List[ExampleColumn], transform_function=None):
+        if transform_function is None:
+            transform_function = self.default_stemming_transform
+        super().__init__(example_columns, transform_function)
+    
+    def default_stemming_transform(self, column: ExampleColumn):
+        ps = PorterStemmer()
+        return [ExampleColumn(attr=ps.stem(column.attr), examples=column.examples)]
+    
